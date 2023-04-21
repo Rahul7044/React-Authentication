767caea595455e522d5488d32eccccd0e4613ed7
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-22 02:33:41 +0530

    Updating the old password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,12 @@
 import { useState, useRef, useContext } from "react";
-import AuthContext from "../../store/auth-context";
+import { useHistory} from 'react-router-dom';
+import AuthContext from "../../store/Auth-Context";
 import classes from "./AuthForm.module.css";
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -52,6 +54,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace('/');
       })
       .catch((err) => {
         alert(err.message);
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,7 +1,7 @@
 import { useContext} from 'react';
 import { Link } from 'react-router-dom';
 
-import AuthContext from '../../store/auth-context';
+import AuthContext from '../../store/Auth-Context';
 
 import classes from './MainNavigation.module.css';
 
